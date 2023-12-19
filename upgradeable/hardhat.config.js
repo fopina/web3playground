dd8aeaa238b8274b921210162f234384fdeb24f5
Filipe Pina <hzlu1ot0@duck.com>
2023-12-19 14:23:01 +0000

    x

--- upgradeable/hardhat.config.js
+++ upgradeable/hardhat.config.js
@@ -1,7 +1,14 @@
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-truffle5");
+const { pkey } = require("./secrets.json")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    fuji: {
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      accounts: [pkey],
+    },
+  },
 };
